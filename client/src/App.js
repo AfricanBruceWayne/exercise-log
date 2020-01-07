3102f1038bfc72eaafd3f7c5b61116d2fd21251c
Thasi Mhlongo <bruce.mhlongo21@gmail.com>
2020-01-07 10:58:56 +0200

    Updated client app files

--- client/src/App.js
+++ client/src/App.js
@@ -1,20 +1,23 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import { Provider } from 'react-redux';
+import jwt_decode from 'jwt-decode';
 import { Container } from 'reactstrap';
 
-import store from './store';
-import jwt_decode from 'jwt-decode';
-import setAuthToken from './setAuthToken';
-import { setCurrentUser, logoutUser } from './actions/authentication';
+import store from './redux/store';
+import setAuthToken from './redux/setAuthToken';
+import { setCurrentUser, logoutUser } from './redux/actions/userActions';
 
-import Navbar from './components/AppNavbar';
+import AppNavbar from './components/AppNavbar';
+import ActivityList from './components/ActivityList';
+import ActivityModal from './components/ActivityModal';
+
 import Register from './pages/Register';
 import Login from './pages/Login';
 import Home from './pages/Home';
-import NewExercise from './pages/NewExercise';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css';
 
 if (localStorage.jwtToken)
 {
@@ -26,22 +29,24 @@ if (localStorage.jwtToken)
     if (decoded.exp < currentTime)
     {
         store.dispatch(logoutUser());
-        window.location.href = '/login'
+        window.location.href = '/'
     }
 }
 
 class App extends Component {
+    
     render() {
         return (
-            <Provider store = {store}>
+            <Provider store={store}>
                 <Router>
-                    <div>
-                        <Navbar />
+                    <div className='App'>
+                        <AppNavbar />
                         <Route exact path="/" component={Home} />
                         <Container>
+                            <ActivityModal />
+                            <ActivityList />
                             <Route exact path="/register" component={Register} />
                             <Route exact path="/login" component={Login} />
-                            <Route exact path="/exercises/new" component={NewExercise} />
                         </Container>
                     </div>
                 </Router>
