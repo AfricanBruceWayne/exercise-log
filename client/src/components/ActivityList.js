6d327970930d1367b764a6047fccb77ef301cd5b
Thasi Mhlongo <bruce.mhlongo21@gmail.com>
2020-01-09 20:47:01 +0200

    Added Table component

--- client/src/components/ActivityList.js
+++ client/src/components/ActivityList.js
@@ -1,8 +1,7 @@
 import React, { Component } from 'react';
 import {
     Container,
-    ListGroup, ListGroupItem,
-    Button
+    Button, Table
 } from 'reactstrap';
 import { CSSTransition, TransitionGroup } from 'react-transition-group';
 import { connect } from 'react-redux';
@@ -24,28 +23,40 @@ class ActivityList extends Component {
 
         return (
           <Container>
-            <ListGroup>
-              <TransitionGroup className='activity-list'>
-                {activities.map(({ _id, date, title, description }) => (
-                  <CSSTransition key={_id} timeout={500} classNames='fade'>
-                    <ListGroupItem>
-                      {this.props.isAuthenticated ? (
-                        <Button
-                          className='remove-btn'
-                          color='danger'
-                          size='sm'
-                          onClick={this.onDeleteClick.bind(this, _id)}
-                        >
-                          &times;
-                        </Button>
-                      ) : null}
-                      {date}
-                      {title} &dash; {description}
-                    </ListGroupItem>
-                  </CSSTransition>
-                ))}
-              </TransitionGroup>
-            </ListGroup>
+            <TransitionGroup className='activity-list'>
+            <Table hover>
+              <thead className="thead-dark">
+                <tr>
+                  <th scope="col">#</th>
+                  <th scope="col">Date</th>
+                  <th scope="col">Description</th>
+                  <th scope="col">Title</th>
+                  <th scope="col">Action</th>
+                </tr>
+              </thead>
+            <tbody>
+              {activities.map(({ _id, date, title, description }) => (
+              <CSSTransition key={_id} timeout={500} classNames='fade'>
+                <tr>
+                  <th scope="row">1</th>
+                  <td>{date}</td>
+                  <td>{description}</td>
+                  <td>{title}</td>
+                  <td>
+                    <Button 
+                        className='remove-btn'
+                        color='danger'
+                        size='sm'
+                        onClick={this.onDeleteClick.bind(this, _id)}>
+                      &times;
+                    </Button>
+                  </td>
+                </tr>
+              </CSSTransition>
+              ))}
+              </tbody>
+            </Table>
+            </TransitionGroup>
           </Container>
         );
     }
