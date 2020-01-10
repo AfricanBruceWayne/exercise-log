a9a196c7b08ab8718d0cfb00e9fe7593e28e9d60
Thasi Mhlongo <bruce.mhlongo21@gmail.com>
2020-01-10 11:17:14 +0200

    Updated client Register page

--- client/src/pages/Register.js
+++ client/src/pages/Register.js
@@ -144,4 +144,4 @@ const mapStateToProps = state => ({
     errors: state.errors
 });
 
-export default connect(mapStateToProps, { registerUser })(withRouter(Register))
+export default connect(mapStateToProps, { registerUser })(withRouter(Register));
