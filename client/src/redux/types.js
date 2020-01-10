6187b1d2f607c9803c17192f6d89cf8ef06e8790
Thasi Mhlongo <bruce.mhlongo21@gmail.com>
2020-01-10 14:01:41 +0200

    Added flash message type

--- client/src/redux/types.js
+++ client/src/redux/types.js
@@ -17,3 +17,4 @@ export const LOADING_UI = 'LOADING_UI';
 export const CLEAR_ERRORS = 'CLEAR_ERRORS';
 export const LOADING_DATA = 'LOADING_DATA';
 export const STOP_LOADING_UI = 'STOP_LOADING_UI';
+export const FLASH_MESSAGE = 'FLASH_MEESAGE';
