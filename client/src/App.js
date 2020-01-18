5b8241ae3dbed48dedf6ca83189cc7a85345d1e8
Thasi Mhlongo <bruce.mhlongo21@gmail.com>
2020-01-18 09:57:38 +0200

    Updated client app

--- client/src/App.js
+++ client/src/App.js
@@ -3,10 +3,13 @@ import { BrowserRouter as Router, Route } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import jwt_decode from 'jwt-decode';
 import { Container } from 'reactstrap';
+import { connect } from 'react-redux';
+
 
 import store from './redux/store';
 import setAuthToken from './redux/setAuthToken';
 import { setCurrentUser, logoutUser } from './redux/actions/userActions';
+import { alertActions } from './redux/actions/alertActions'
 
 import AppNavbar from './components/AppNavbar';
 import ActivityModal from './components/ActivityModal';
@@ -17,6 +20,10 @@ import Home from './pages/Home';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
+
+import { createBrowserHistory } from 'history';
+
+export const history = createBrowserHistory();
 
 if (localStorage.jwtToken)
 {
@@ -32,14 +39,38 @@ if (localStorage.jwtToken)
     }
 }
 
+
+
 class App extends Component {
     
+    constructor(props) {
+        super(props);
+
+        const { dispatch } = this.props;
+        history.listen((location, action) => {
+            // clear alert on location change
+            dispatch(alertActions.clear());
+        });
+    }
+
     render() {
+
+        const { alert } = this.props;
+
         return (
             <Provider store={store}>
                 <Router>
                     <div className='App'>
                         <AppNavbar />
+                        <div className="container mx-auto mb-3">
+                            <div className="col-sm-8 col-sm-offset-2">
+                                {
+                                    alert.message && <div className={`alert ${alert.type}`}>
+                                        {alert.message}
+                                    </div>
+                                }
+                            </div>
+                        </div>
                         <ActivityModal />
                         <Route exact path="/" component={Home} />
                         <Container>
@@ -53,4 +84,8 @@ class App extends Component {
     }
 }
 
-export default App;
+const mapStateToProps = state => ({
+    alert: state.alert
+})
+
+export default connect(mapStateToProps)(App); 
